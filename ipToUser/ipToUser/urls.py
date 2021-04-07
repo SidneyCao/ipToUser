bf75eed4960f0472dd79aaedc51091139e2980eb
Simoncao <13671707129@126.com>
2021-04-07 16:15:26 +0800

    [fix]add index

--- ipToUser/ipToUser/urls.py
+++ ipToUser/ipToUser/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('admin/', admin.site.urls)
 ]
