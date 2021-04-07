3872faded9215db4f13788c8066c4bc02fe49547
Simoncao <13671707129@126.com>
2021-04-07 18:25:38 +0800

    [debug]utp function

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -17,5 +17,5 @@ def userToip(user):
         for line in f:
             if re.search(pattern, line):
                 lastMatch = line   
-    print(lastMatch.split(' '))    
+    print(lastMatch.split(' ')[-1].split(')')[0])    
 
