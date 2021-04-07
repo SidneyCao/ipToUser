a59b5332e70eb3f3c852c4f590e50b7bb39e3dfb
Simoncao <13671707129@126.com>
2021-04-07 18:06:21 +0800

    [feat]utp function

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+import re
+
 
 def index(request):
     user = request.GET.get('user')
-    return HttpResponse(user)
+    ip = request.GET.get('ip')
+    if user != None:
+        userToip(user)
+        return HttpResponse(user)
+    else:
+        return HttpResponse(ip)
+
+def userToip(user):
+    pattern = user
+    with open('/var/log/radius/radius.log') as f:
+        for line in f:
+            if re.search(pattern, line):
+                print(line)
+            
+
