0906d4a85e90915a40c08792d01fe6849b7ad251
Simoncao <13671707129@126.com>
2021-04-09 17:45:18 +0800

    [feat]utm module

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -41,7 +41,7 @@ def sshToFind(key: str, value: str, offset: int):
     stdin, stdout, stderr = client.exec_command('ip dhcp-server lease print where {}={}'.format(key,value))
     ans = ''
     for line in stdout:
-        if re.match(value, line):
+        if re.search(value, line):
             print(line)
             ans = line.split(' ')[offset]
     return ans
