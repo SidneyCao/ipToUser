3f0157d261fa0b0d283b8ec89da6f043de43cbf6
Simoncao <13671707129@126.com>
2021-04-09 13:41:33 +0800

    [feat]utm module

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponse
+from datetime import date
 import re
+import paramiko
 
+today = date.today.strftime("%b  %d")
 
 def index(request):
     user = request.GET.get('user')
     ip = request.GET.get('ip')
     if user != None:
-        mac = userToip(user)
+        mac = userToMac(user)
         return HttpResponse(mac)
     else:
         return HttpResponse(ip)
 
-def userToip(user):
+
+def userToMac(user):
     pattern = user
+    print(today)
     with open('/var/log/radius/radius.log') as f:
         for line in f:
             if re.search(pattern, line):
                 lastMatch = line   
     return lastMatch.split(' ')[-1].split(')')[0].replace('-',':')    
 
+def macToip(mac):
