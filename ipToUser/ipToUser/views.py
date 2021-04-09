c1d63e1e0fbecd32a30969871584155389c4b4cf
Simoncao <13671707129@126.com>
2021-04-09 14:31:09 +0800

    [feat]utm module

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -16,7 +16,7 @@ def index(request):
 
 
 def userToMac(user: str) -> set:
-    pattern = '^.*' + today + '.*' + user + '.*cli+*$'
+    pattern = '^.*' + today + '.*' + user + '.*cli+$'
     mac = set()
     with open('/var/log/radius/radius.log') as f:
         for line in f:
