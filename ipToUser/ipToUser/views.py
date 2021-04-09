e66ae81fccab9bfdc8fe776db771f2446f659adc
Simoncao <13671707129@126.com>
2021-04-09 14:51:07 +0800

    [feat]utm module

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -10,7 +10,7 @@ def index(request):
     ip = request.GET.get('ip')
     if user != None:
         mac = userToMac(user)
-        return HttpResponse(mac)
+        return HttpResponse(mac,content_type="text/plain")
     else:
         return HttpResponse(ip)
 
