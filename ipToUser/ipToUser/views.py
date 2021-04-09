7bfb9ac030e57f4e881d6addb2e528e6dbc6f2d6
Simoncao <13671707129@126.com>
2021-04-09 16:14:15 +0800

    [feat]utm module

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -9,17 +9,21 @@ def index(request):
     user = request.GET.get('user')
     ip = request.GET.get('ip')
     if user != None:
-        mac = userToMac(user)
-        return HttpResponse(mac,content_type="text/plain")
+        macs = userToMac(user)
+        return HttpResponse(macs.keys(),content_type="text/plain")
     else:
         return HttpResponse(ip)
 
 
-def userToMac(user: str) -> set:
+def userToMac(user: str) -> dict:
     pattern = '^.*' + today + '.*' + user + '.*cli\s+.*$'
-    mac = set()
+    macs = {}
     with open('/var/log/radius/radius.log') as f:
         for line in f:
             if re.match(pattern, line): 
-                mac.add(line.split('cli ')[-1].split(')')[0].replace('-',':')+'\n')    
-    return mac
+                mac = line.split('cli ')[-1].split(')')[0].replace('-',':')+'\n'
+                if mac in macs:
+                    pass
+                else:
+                    macs[mac]=''
+    return macs
