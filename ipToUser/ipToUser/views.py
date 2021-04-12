92a78bab1554f604dddeaa71c64b10feb3daf6e0
Simoncao <13671707129@126.com>
2021-04-12 11:38:52 +0800

    [feat]utm module

--- ipToUser/ipToUser/views.py
+++ ipToUser/ipToUser/views.py
@@ -40,9 +40,8 @@ def sshToFind(key: str, value: str, offset: int):
     client.connect(hostname=DHCPServ,port=22,username='admin',pkey=pKey)
     stdin, stdout, stderr = client.exec_command('ip dhcp-server lease print where {}={}'.format(key,value))
     ans = ''
-    for line in stdout.read().decode('utf-8'):
-        ans = line.replace(' ','')
-        print(line)
+    print(stdout.read().decode('utf-8'))
+        
     
     client.close()
     return ans
